cceaa3d2ae2d4fa40298323f5df342eff035ae5a
Lucas E. Oliaro Vera <lucasoliaro@gmail.com>
2023-11-11 22:28:03 -0300

    Refactor head and tail methods in Selection.java

--- src/utils_df/Selection.java
+++ src/utils_df/Selection.java
@@ -1,47 +1,56 @@
 package utils_df;
 
-import dataframe.DataFrame;
 import dataframe.Column;
+import dataframe.DataFrame;
 import dataframe.cells.Cell;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * La clase Selection proporciona métodos para seleccionar filas de un DataFrame.
- * Los métodos head y tail devuelven un nuevo DataFrame que contiene las primeras o últimas filas del DataFrame original, respectivamente.
+ * Clase que contiene métodos para seleccionar filas de un DataFrame.
  */
 public class Selection {
-    public static DataFrame head(DataFrame df) {
 
-        DataFrame dfHead = new DataFrame();
-    
-        int numRows = Math.min(5, df.getCantFilas()); // Evita desbordamiento si hay menos de 5 filas
-    
-        for (int i = 0; i < df.getCantColumnas(); i++) {
-            Column column = df.getColumn(i);
-            List<Cell> cells = column.getContent();
-            List<Cell> newColumnCells = new ArrayList<>(cells.subList(0, numRows));
-            dfHead.addColumn(newColumnCells);
-        }
-    
-        return dfHead;
+  /**
+   * Retorna un nuevo DataFrame con las primeras 5 filas de cada columna del DataFrame original.
+   * @param df El DataFrame original.
+   * @return Un nuevo DataFrame con las primeras 5 filas de cada columna del DataFrame original.
+   */
+  public static DataFrame head(DataFrame df) {
+    DataFrame dfHead = new DataFrame();
+
+    int numRows = Math.min(5, df.getCantFilas()); // Evita desbordamiento si hay menos de 5 filas
+
+    for (int i = 0; i < df.getCantColumnas(); i++) {
+      Column column = df.getColumn(i);
+      List<Cell> cells = column.getContent();
+      List<Cell> newColumnCells = new ArrayList<>(cells.subList(0, numRows));
+      dfHead.addColumn(newColumnCells);
     }
-    
-    public static DataFrame tail(DataFrame df) {
-        DataFrame dfTail = new DataFrame();
-    
-        int numRows = Math.min(5, df.getCantFilas()); // Evita desbordamiento si hay menos de 5 filas
-    
-        for (int i = 0; i < df.getCantColumnas(); i++) {
-            Column column = df.getColumn(i);
-            List<Cell> cells = column.getContent();
-            int startIndex = Math.max(0, cells.size() - numRows); // Asegura que no se inicie desde un índice negativo
-            List<Cell> newColumnCells = new ArrayList<>(cells.subList(startIndex, cells.size()));
-            dfTail.addColumn(newColumnCells);
-        }
-    
-        return dfTail;
-    }   
-    
-    
+
+    return dfHead;
+  }
+
+  /**
+   * Retorna un nuevo DataFrame con las últimas 5 filas de cada columna del DataFrame original.
+   * @param df El DataFrame original.
+   * @return Un nuevo DataFrame con las últimas 5 filas de cada columna del DataFrame original.
+   */
+  public static DataFrame tail(DataFrame df) {
+    DataFrame dfTail = new DataFrame();
+
+    int numRows = Math.min(5, df.getCantFilas()); // Evita desbordamiento si hay menos de 5 filas
+
+    for (int i = 0; i < df.getCantColumnas(); i++) {
+      Column column = df.getColumn(i);
+      List<Cell> cells = column.getContent();
+      int startIndex = Math.max(0, cells.size() - numRows); // Asegura que no se inicie desde un índice negativo
+      List<Cell> newColumnCells = new ArrayList<>(
+        cells.subList(startIndex, cells.size())
+      );
+      dfTail.addColumn(newColumnCells);
+    }
+
+    return dfTail;
+  }
 }
