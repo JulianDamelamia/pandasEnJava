b2a5a8ac9efa98be3252ccb1ccce109adb333eca
Lucas E. Oliaro Vera <lucasoliaro@gmail.com>
2023-11-11 22:28:34 -0300

    Refactor Column class to improve readability and maintainability.

--- src/dataframe/Column.java
+++ src/dataframe/Column.java
@@ -1,84 +1,138 @@
 package dataframe;
 
+import dataframe.cells.Cell;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
-import dataframe.cells.BooleanCell;
-import dataframe.cells.Cell;
-
+/**
+ * Clase que representa una columna de un DataFrame.
+ */
 public class Column {
-    private int size;
-    private ArrayList<Cell> content;
-    //Constructor vacio
-    public Column() {
-        this.size = 0;
-        this.content = new ArrayList<Cell>();
-    }
-    public Column(int size) {
-        this.size = size;
-        this.content = new ArrayList<Cell>();
-    }
 
-    public Column (ArrayList<Cell> content) {
-        this.size = content.size();
-        this.content = content;
-    }
-    public Column(int size, ArrayList<Cell> content) {
-        this.size = size;
-        this.content = content;
-    }
+  private int size;
+  private ArrayList<Cell> content;
 
-    private Column(Column original) {
-        this(original.size(), original.getContent());       
-    }
-    public Column copy() {
-        Column copyColumn = new Column(this);
-        copyColumn.content = new ArrayList<Cell>(size);
-        for (Cell cell : this.content) {
-            Cell copyCell = cell.copy();
-            copyColumn.content.add(copyCell);
-        }
-        return copyColumn;
-    }
+  /**
+   * Constructor vacío que inicializa una columna sin elementos.
+   */
+  public Column() {
+    this.size = 0;
+    this.content = new ArrayList<Cell>();
+  }
 
-    public void setCell(int index, Object value){
-            this.content.get(index).setValue(value);
-        }
+  /**
+   * Constructor que inicializa una columna con un tamaño específico.
+   * @param size el tamaño de la columna.
+   */
+  public Column(int size) {
+    this.size = size;
+    this.content = new ArrayList<Cell>();
+  }
 
-    //probablemente quiera esto protected para no agregar arrays de largo inválido una vez creado el df
-    public void setContent(ArrayList<Cell> content){
-        this.content =content;
-        this.size = content.size();
+  /**
+   * Constructor que inicializa una columna con una lista de celdas.
+   * @param content la lista de celdas que conforman la columna.
+   */
+  public Column(ArrayList<Cell> content) {
+    this.size = content.size();
+    this.content = content;
+  }
+
+  /**
+   * Constructor que inicializa una columna con un tamaño específico y una lista de celdas.
+   * @param size el tamaño de la columna.
+   * @param content la lista de celdas que conforman la columna.
+   */
+  public Column(int size, ArrayList<Cell> content) {
+    this.size = size;
+    this.content = content;
+  }
+
+  /**
+   * Constructor privado que inicializa una columna a partir de otra columna.
+   * @param original la columna original a partir de la cual se creará la nueva columna.
+   */
+  private Column(Column original) {
+    this(original.size(), original.getContent());
+  }
+
+  /**
+   * Crea una copia de la columna actual.
+   * @return una nueva columna que es una copia de la columna actual.
+   */
+  public Column copy() {
+    Column copyColumn = new Column(this);
+    copyColumn.content = new ArrayList<Cell>(size);
+    for (Cell cell : this.content) {
+      Cell copyCell = cell.copy();
+      copyColumn.content.add(copyCell);
     }
+    return copyColumn;
+  }
 
-    
-    
-    public Cell getCell(int index){
-        return this.content.get(index);
+  /**
+   * Establece el valor de una celda en la posición especificada.
+   *
+   * @param index la posición de la celda a establecer.
+   * @param value el valor a establecer en la celda.
+   */
+  public void setCell(int index, Object value) {
+    this.content.get(index).setValue(value);
+  }
+
+  /**
+   * Establece el contenido de la columna a partir de una lista de celdas.
+   * @param content la lista de celdas que conformarán la columna.
+   */
+  public void setContent(ArrayList<Cell> content) {
+    this.content = content;
+    this.size = content.size();
+  }
+
+  /**
+   * Obtiene la celda en la posición especificada.
+   * @param index la posición de la celda a obtener.
+   * @return la celda en la posición especificada.
+   */
+  public Cell getCell(int index) {
+    return this.content.get(index);
+  }
+
+  /**
+   * Obtiene el tamaño de la columna.
+   * @return el tamaño de la columna.
+   */
+  public int size() {
+    return this.size;
+  }
+
+  /**
+   * Constructor que inicializa una columna con una lista de celdas.
+   * @param cells la lista de celdas que conforman la columna.
+   */
+  public Column(List<Cell> cells) {
+    this.size = cells.size();
+    this.content = new ArrayList<>(cells);
+  }
+
+  /**
+   * Obtiene el contenido de la columna.
+   * @return el contenido de la columna.
+   */
+  public ArrayList<Cell> getContent() {
+    return this.content;
+  }
+
+  /**
+   * Devuelve una representación en cadena de la columna.
+   * @return una representación en cadena de la columna.
+   */
+  @Override
+  public String toString() {
+    String result = "";
+    for (Cell cell : this.content) {
+      result += cell.toString() + " ";
     }
-
-    public int size(){
-        return this.size;
-    }
-    
-    public Column(List<Cell> cells) {
-        this.size = cells.size();
-        this.content = new ArrayList<>(cells);
-    }
-
-
-    public ArrayList<Cell> getContent(){
-        return this.content;
-    }
-    @Override
-    public String toString(){
-        String result = "";
-        for (Cell cell : this.content) {
-            result += cell.toString() + " ";
-        }
-        return result;
-    }
-
-
+    return result;
+  }
 }
