de6336785c1763ccd8131c8b247584c5bd65bd84
Julian <jlfraga@estudiantes.unsam.edu.ar>
2023-11-16 19:39:12 -0300

    feature: filtro con operadores lógicos andando

--- src/Main.java
+++ src/Main.java
@@ -165,14 +165,17 @@ public class Main {
         // System.out.println(copySortedColumna.columnOrderMap);
         copySortedColumna.show();
 
-        System.out.println("seleccion");
-        DataFrame seleccion = dfCopy.select( new String[]{"0", "1"}, new String[]{"TORTUGA3", "NUMERICA"});
+        // System.out.println("seleccion");
+        // DataFrame seleccion = dfCopy.select( new String[]{"0", "1"}, new String[]{"TORTUGA3", "NUMERICA"});
         // System.out.println("Columnas seleccionadas: Tortuga3 y numerica");
         // System.out.println("filas seleccionadas: 0 y 1 \n");
         // System.out.println("roworder original "+dfCopy.rowOrderMap);
         // System.out.println("roworder seleccion "+seleccion.rowOrderMap+ "\n");
         // System.out.println("colOrder original "+dfCopy.columnOrderMap);
         // System.out.println("colOrder seleccion "+seleccion.columnOrderMap );
-        seleccion.show();
+        // seleccion.show();
+        System.out.println("\n Filtrado");
+        DataFrame filtrado = df.filter("TORTUGA3", ">=", "beta");
+        filtrado.show();
      }
 }
---
+++ src/utils_df/Criterios.java
@@ -0,0 +1,22 @@
+package utils_df;
+
+import java.util.HashMap;
+
+import java.util.Map;
+import java.util.function.Predicate;
+
+import dataframe.cells.Cell;
+
+
+public class Criterios {
+    public Map<String, Predicate<Cell>> operadores;
+    public Criterios(Cell valor){
+        operadores = new HashMap<>();
+        operadores.put("<", e -> e.compareTo(valor) < 0);
+        operadores.put(">", e -> e.compareTo(valor) > 0);
+        operadores.put("=", e -> e.compareTo(valor) == 0);
+        operadores.put("!" , e-> e.compareTo(valor) != 0 );
+        operadores.put("<=", e -> e.compareTo(valor) <= 0);
+        operadores.put(">=", e -> e.compareTo(valor) >= 0);
+    } 
+}
